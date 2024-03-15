9660f581744f98d55248ec052f83a8faef95d105
fyzpkl <feyizpekel@gmail.com>
2024-03-15 12:08:19 -0400

    event message5

--- src/basicTreeView.js
+++ src/basicTreeView.js
@@ -10,11 +10,13 @@ function BasicTreeView() {
       console.log('Source:', event.data.source);
 
       if (event.origin !== "https://enterprise-force-7539--partialsb.sandbox.my.salesforce.com/services/apexrest/") {
+        console.log('Invalid origin:', event.origin);
         // Validate the message origin
         return;
       }
 
       if (event.data && event.data.source === 'SalesforceLWC') {
+        console.log('Received Salesforce LWC message:', event.data.recordId);
         setReceivedMessage(event.data.recordId);
         // Additional logic for handling the received message
       }
@@ -23,6 +25,8 @@ function BasicTreeView() {
     window.addEventListener('message', handleMessage);
     return () => window.removeEventListener('message', handleMessage);
   }, []);
+
+  console.log('Received Message:', receivedMessage);
 
   return (
     <div>
