b25e4053c5cb7950fa70ee5fc0d12e68c5f66175
fyzpkl <feyizpekel@gmail.com>
2024-03-15 11:17:56 -0400

    initial commit 4

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import BasicTreeView from './basicTreeView'; // Update the import to your BasicT
 
 function App() {
   return (
-    <div>
-        <input 
-            type="text" 
-            value={recordId} 
-            onChange={(e) => setRecordId(e.target.value)} 
-            placeholder="Enter Record ID" 
-        />
-        <button onClick={handleFetchData}>Fetch Data</button>
-
-        <Tree data={treeData} />
+    <div style={{ padding: '20px' }}>
+      <h1>My React Tree View</h1>
+      <BasicTreeView/>
     </div>
   );
 }
