dc2ffe0cf5cc2cd286cca53c9d93c12080136371
fyzpkl <feyizpekel@gmail.com>
2024-03-15 12:03:53 -0400

    event message4

--- src/basicTreeView.js
+++ src/basicTreeView.js
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
-function App() {
-    const [recordId, setRecordId] = useState('');
+function BasicTreeView() {
+  const [receivedMessage, setReceivedMessage] = useState('');
 
-    useEffect(() => {
-      const handleMessage = (event) => {
-          console.log('Received message:', event);
-          console.log('Data:', event.data);
-          console.log('Source:', event.data.source);
-  
-          if (event.origin !== "https://enterprise-force-7539--partialsb.sandbox.my.salesforce.com/services/apexrest/") {
-              // Validate the message origin
-              return;
-          }
-  
-          if (event.data && event.data.source === 'SalesforceLWC') {
-              setRecordId(event.data.recordId);
-              // Additional logic for handling the recordId
-          }
-      };
-  
-      window.addEventListener('message', handleMessage);
-      return () => window.removeEventListener('message', handleMessage);
+  useEffect(() => {
+    const handleMessage = (event) => {
+      console.log('Received message:', event);
+      console.log('Data:', event.data);
+      console.log('Source:', event.data.source);
+
+      if (event.origin !== "https://enterprise-force-7539--partialsb.sandbox.my.salesforce.com/services/apexrest/") {
+        // Validate the message origin
+        return;
+      }
+
+      if (event.data && event.data.source === 'SalesforceLWC') {
+        setReceivedMessage(event.data.recordId);
+        // Additional logic for handling the received message
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
   }, []);
 
-      return (
-        <div>
-            {/* Your component UI */}
-            {recordId && <div>Received Record ID: {recordId}</div>}
-            {/* Additional UI elements */}
-        </div>
-    );
+  return (
+    <div>
+      {/* Render the received message */}
+      {receivedMessage && <div>Received Message: {receivedMessage}</div>}
+      {/* Additional UI elements */}
+    </div>
+  );
 }
 
-export default App;
+export default BasicTreeView;
