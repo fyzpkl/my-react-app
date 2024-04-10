30294da6477bf478937ba217d958d01eb80ba50b
fyzpkl <feyizpekel@gmail.com>
2024-04-10 10:41:14 -0400

    added handled by and company dynamically

--- src/basicTreeView.js
+++ src/basicTreeView.js
@@ -66,24 +66,22 @@ function BasicTreeView() {
       "handled_by": handledById, 
       "company_id": companyId
     };
-    console.log('companyIdy' + handledById);
-    console.log('companyId' + companyId);
-    // try {
-    //   const response = await fetch('https://mk-be-0f3c24a58a9b.herokuapp.com/run_submission_group', {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(requestBody)
-    //   });
-    //   const data = await response.json();
-    //   setApiResponse(JSON.stringify(data, null, 2));
-    // } catch (error) {
-    //   console.error('Error running submission:', error);
-    //   setApiResponse('Error: Could not complete submission'); 
-    // } finally {
-    //   setIsSubmitting(false);
-    //   setButtonClicked(true);
+    try {
+      const response = await fetch('https://mk-be-0f3c24a58a9b.herokuapp.com/run_submission_group', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(requestBody)
+      });
+      const data = await response.json();
+      setApiResponse(JSON.stringify(data, null, 2));
+    } catch (error) {
+      console.error('Error running submission:', error);
+      setApiResponse('Error: Could not complete submission'); 
+    } finally {
+      setIsSubmitting(false);
+      setButtonClicked(true);
 
-    // }
+    }
    };
   const renderGridItems = (nodes, level = 0) => {
     if (!nodes) return null;
